46e3ed2c7b54938918c098aac08244d063560270
James LeVasseur <jameslevasseur@Jamess-MacBook-Pro.local>
2017-08-08 21:16:23 -0400

    removing farts:

--- shipitfile.js
+++ shipitfile.js
@@ -3,34 +3,17 @@ module.exports = function (shipit) {
 
   shipit.initConfig({
     default: {
-      workspace: './',
+      workspace: '.',
       deployTo: '/var/www/html/deploy_test',
-      ignores: ['.git', 'node_modules'],
-      rsync: ['--del'],
-      keepReleases: 2,
-      shallowClone: true
-    },
-    staging: {
-      servers: '67.251.12.204'
-    }
-  });
-};
-module.exports = function (shipit) {
-  require('shipit-deploy')(shipit);
-
-  shipit.initConfig({
-    default: {
-      workspace: '/tmp/github-monitor',
-      deployTo: '/tmp/deploy_to',
       repositoryUrl: 'https://github.com/jamezlevasseur/IMRC_AM_2k17.git',
       ignores: ['.git', 'node_modules'],
       keepReleases: 2,
       deleteOnRollback: false,
-      key: '/path/to/key',
+      key: '~./ssh/id_rsa',
       shallowClone: true
     },
     staging: {
       servers: 'andrewryan@67.251.12.204'
     }
   });
-};farts
+};
