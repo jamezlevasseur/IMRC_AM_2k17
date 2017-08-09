c6184e5cc572c0bca1a6a1edc46104a9d07df0fa
James LeVasseur <jameslevasseur@Jamess-MacBook-Pro.local>
2017-08-08 21:10:08 -0400

    testing new key

--- shipitfile.js
+++ shipitfile.js
@@ -15,3 +15,22 @@ module.exports = function (shipit) {
     }
   });
 };
+module.exports = function (shipit) {
+  require('shipit-deploy')(shipit);
+
+  shipit.initConfig({
+    default: {
+      workspace: '/tmp/github-monitor',
+      deployTo: '/tmp/deploy_to',
+      repositoryUrl: 'https://github.com/jamezlevasseur/IMRC_AM_2k17.git',
+      ignores: ['.git', 'node_modules'],
+      keepReleases: 2,
+      deleteOnRollback: false,
+      key: '/path/to/key',
+      shallowClone: true
+    },
+    staging: {
+      servers: 'andrewryan@67.251.12.204'
+    }
+  });
+};farts
