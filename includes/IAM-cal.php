b07dbec7c8b1b03d8140f74f666d7afa4cff0d8f
jamezlevasseur <jlevimportant@gmail.com>
2017-09-17 23:35:03 -0400

    catching up to dev

--- includes/IAM-cal.php
+++ includes/IAM-cal.php
@@ -5,61 +5,6 @@
 */
 class IAM_Cal
 {
-	public static function update_equipment_cal()
-	{
-		global $wpdb;
-		/*
-		if (isset($_POST['new_events'])) {
-			foreach ($_POST['new_events'] as $key => $value) {
-
-			}
-		}*/
-		if (isset($_POST['modified'])) {
-			foreach ($_POST['modified'] as $key => $value) {
-				if (in_array($key, $_POST['to_delete']))
-					continue;
-				$reservation_results = $wpdb->get_results($wpdb->prepare("SELECT IAM_ID, Equipment_ID, Start_Time FROM ".IAM_RESERVATION_TABLE." WHERE NI_ID=%s", IAM_Sec::textfield_cleaner($key)));
-				$iam_id = $reservation_results[0]->IAM_ID;
-				$equip_id = $reservation_results[0]->Equipment_ID;
-				$wp_id = $wpdb->get_results($wpdb->prepare("SELECT WP_ID FROM ".IAM_USERS_TABLE." WHERE IAM_ID=%d",$iam_id))[0]->WP_ID;
-				$equip_name = $wpdb->get_results($wpdb->prepare("SELECT Name FROM ".IAM_EQUIPMENT_TABLE." WHERE Equipment_ID=%d",$equip_id))[0]->Name;
-				$wpdb->query($wpdb->prepare("UPDATE ".IAM_RESERVATION_TABLE." SET Start_Time=%s, End_Time=%s WHERE NI_ID=%s",$value['start'],$value['end'],$key));
-				if ($_POST['sendEmails']=='true') {
-					$reason = $_POST['reason']=='' ? '' : ' Reason given: '.$_POST['reason'];
-
-					$old_time = DateTime::createFromFormat('Y-m-d H:i:s',$reservation_results[0]->Start_Time);
-					$old_time = $old_time->format('M d, Y \a\t g:i a');
-
-					$new_start = DateTime::createFromFormat('Y-m-d H:i:s',$value['start']);
-					$new_start = $new_start->format('M d, Y \a\t g:i a');
-
-					$new_end = DateTime::createFromFormat('Y-m-d H:i:s',$value['end']);
-					$new_end = $new_end->format('M d, Y \a\t g:i a');
-					iam_mail(get_userdata($wp_id)->user_email, 'Your IMRC reservation', 'Your reservation for '.$equip_name.' at '.$old_time.' has been changed. The new time is from '.$new_start.' to '.$new_end.'.'.$reason,'Failed to send update appointment email');
-				}
-			}
-		}
-		if (isset($_POST['to_delete'])) {
-			for ($i=0; $i < count($_POST['to_delete']); $i++) {
-				$ni_id = IAM_Sec::textfield_cleaner($_POST['to_delete'][$i]);
-				$reservation_results = $wpdb->get_results($wpdb->prepare("SELECT IAM_ID, Equipment_ID, Start_Time FROM ".IAM_RESERVATION_TABLE." WHERE NI_ID=%s", $ni_id));
-				$iam_id = $reservation_results[0]->IAM_ID;
-				$equip_id = $reservation_results[0]->Equipment_ID;
-				$wp_id = $wpdb->get_results($wpdb->prepare("SELECT WP_ID FROM ".IAM_USERS_TABLE." WHERE IAM_ID=%d",$iam_id))[0]->WP_ID;
-				$equip_name = $wpdb->get_results($wpdb->prepare("SELECT Name FROM ".IAM_EQUIPMENT_TABLE." WHERE Equipment_ID=%d",$equip_id))[0]->Name;
-				$start_time = $reservation_results[0]->Start_Time;
-				$wpdb->query($wpdb->prepare("DELETE FROM ".IAM_RESERVATION_TABLE." WHERE NI_ID=%s", $ni_id));
-				if ($_POST['sendEmails']=='true') {
-					$reason = $_POST['reason']=='' ? '' : ' Reason given: '.$_POST['reason'];
-					$old_time = DateTime::createFromFormat('Y-m-d H:i:s',$reservation_results[0]->Start_Time);
-					$old_time = $old_time->format('M d, Y \a\t g:i a');
-					iam_mail(get_userdata($wp_id)->user_email, 'Your IMRC reservation', 'Your reservation for '.$equip_name.' at '.$old_time.' has been cancelled by an IMRC administrator.'.$reason,'Failed to send delete appointment email');
-				}
-			}
-		}
-
-		iam_respond(SUCCESS,implode(",",$_POST['to_delete']),IAM::$status_message);
-	}
 
 	public static function get_business_hours()
 	{
@@ -132,8 +77,8 @@ class IAM_Cal
 			$equip_id = $equip_result[0]->Equipment_ID;
 
 			date_default_timezone_set(IMRC_TIME_ZONE);
-			$weekago = date("Y-m-d 00:00:00", strtotime('-4 week'));
-			$date_condition = $get_all_reservations ? " " : " AND Start_Time > '$weekago' ";
+			$monthago = short_res_period();
+			$date_condition = $get_all_reservations ? " " : " AND Start_Time > '$monthago' ";
 			$noshow_condition = $is_admin ? "" : " AND Status!=".NO_SHOW;
 			$res_result = $wpdb->get_results($wpdb->prepare("SELECT * FROM ".IAM_RESERVATION_TABLE." WHERE Equipment_ID=%d AND Is_Room='0'".$date_condition.$noshow_condition, $equip_id));
 			$formatted_events = [];
@@ -189,33 +134,11 @@ class IAM_Cal
 
 	public static function get_equipment_cal()
 	{
-		if (isset($_GET['items'])) {
-			/*$names = $_GET['items'];
-			//print_r($names);exit;
-			if (gettype($names)!='array') {
-				iam_throw_error(INVALID_INPUT_EXCEPTION);
-			}
-			$a = [];
-			for ($i=0; $i < count($names); $i++) {
-				if (trim($names[$i])=='') {
-					continue;
-				}
-				$item_name = str_replace('_', ' ', IAM_Sec::textfield_cleaner( $names[$i] ));
-
-				$item_events = IAM_Cal::get_cal_for_equipment($item_name);
-				$item_events = $item_events===null ? [] : $item_events;
-				$a = array_merge($a,$item_events);
-			}
-
-			echo json_encode($a);
-			exit;*/
-		} else if (isset($_GET['name'])) {
+		if (isset($_GET['name'])) {
 			$item_name = str_replace('_', ' ', IAM_Sec::textfield_cleaner( $_GET['name'] ));
 			echo json_encode(IAM_Cal::get_cal_for_equipment($item_name, $_GET));
 			exit;
 		}
-
-
 	}
 
 }
